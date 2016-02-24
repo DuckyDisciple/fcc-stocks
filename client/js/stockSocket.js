db3b829f2410d26a70e8dff712a9d0586f229376
duckydisciple <duckydesign@gmail.com>
2016-02-24 14:30:31 -0500

    Sockets finished

--- client/js/stockSocket.js
+++ client/js/stockSocket.js
@@ -1,8 +1,10 @@
 var stockName = document.querySelector(".stock-name") || null;
 var stockSymbol = document.querySelector(".stock-symbol") || null;
+var userUL = document.querySelector(".watching-list") || null;
 
 var watchUrl = appUrl + '/api/watch/';
 var unwatchUrl = appUrl + '/api/unwatch/';
+var userListUrl = appUrl + '/api/users/';
 
 var socket = io();
 $(".watch").click(function(){
@@ -25,3 +27,26 @@ $(".unwatch").click(function(){
         $(".unwatch").addClass("hide");
     });
 });
+
+socket.on("update",function(msg){
+   ajaxFunctions.ready(ajaxFunctions.ajaxRequest("GET",userListUrl+stockSymbol.innerHTML,function(data) {
+            updateWatchersList(JSON.parse(data),userUL);
+        })); 
+});
+
+function updateWatchersList(userList,element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
+    }
+    if(userList.length === 0){
+        var nobody = document.createElement("li");
+        nobody.innerHTML = "Nobody is watching this yet";
+        element.appendChild(nobody);
+    }else{
+        for(var i=0; i< userList.length; i++){
+            var item = document.createElement("li");
+            item.innerHTML=userList[i];
+            element.appendChild(item);
+        }
+    }
+}
